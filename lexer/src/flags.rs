d037a2dd3c609aceeb9af5f0b75472beea3a9bc8
Eucladia <eucladialyra@gmail.com>
2024-09-04 16:08:31 -0400

    refactor: remove unsed `Flags` functions

--- lexer/src/flags.rs
+++ lexer/src/flags.rs
@@ -11,22 +11,3 @@ pub enum Flags {
   /// Set if the resulting operation produced a carry over the nibbles.
   AuxiliaryCarry = 1 << 4,
 }
-
-impl Flags {
-  /// Whether or the given string is a [Flags].
-  pub fn is_flag(string: &str) -> bool {
-    Self::from_string(string).is_some()
-  }
-
-  /// Parses a [Flags] from a string.
-  pub fn from_string(string: &str) -> Option<Self> {
-    match string {
-      string if string.eq_ignore_ascii_case("zero") => Some(Flags::Zero),
-      string if string.eq_ignore_ascii_case("carry") => Some(Flags::Carry),
-      string if string.eq_ignore_ascii_case("sign") => Some(Flags::Sign),
-      string if string.eq_ignore_ascii_case("parity") => Some(Flags::Parity),
-      string if string.eq_ignore_ascii_case("auxiliarycarry") => Some(Flags::AuxiliaryCarry),
-      _ => None,
-    }
-  }
-}
