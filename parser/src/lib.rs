72bb4a22cf7a1fcc76eeb9d1b248b14c0abac4f5
Eucladia <eucladialyra@gmail.com>
2025-01-02 13:52:26 -0500

    parser: add convenience parse functions

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,6 +1,10 @@
 pub mod nodes;
 pub mod parser;
 
+use lexer::Token;
+use nodes::ProgramNode;
+use types::ParseResult;
+
 #[macro_export]
 macro_rules! unwrap {
   ($expr:expr) => {{
@@ -13,4 +17,20 @@ macro_rules! unwrap {
       $expr.unwrap_unchecked()
     }
   }};
+}
+
+pub fn parse_tokens(src: &str, tokens: Vec<Token>) -> ParseResult<ProgramNode> {
+  let mut parser = parser::Parser::new(src, tokens);
+
+  parser.parse()
+}
+
+pub fn parse(src: &str) -> types::Result<ProgramNode> {
+  match lexer::lex(src) {
+    Ok(tokens) => match parse_tokens(src, tokens) {
+      Ok(node) => Ok(node),
+      Err(err) => Err(types::Error::Parser(err)),
+    },
+    Err(err) => Err(types::Error::Lexer(err)),
+  }
 }
